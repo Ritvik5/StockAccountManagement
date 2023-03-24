63732bf11ca0f4dc7a16136da6a4923c92d5c952
Ritvik5 <sharma.ritvik56@gmail.com>
2023-03-24 17:59:57 +0530

    Stock Report

---
+++ StockAccountManagement/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace StockAccountManagement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of stocks: ");
+            int numStocks = int.Parse(Console.ReadLine());
+
+
+            StockPortfolio portfolio = new StockPortfolio();
+
+            for (int i = 1; i <= numStocks; i++)
+            {
+                Console.WriteLine("\n**************\n");
+                Console.Write("Enter the stock name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Enter the number of shares: ");
+                int numShares = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter the share price: ");
+                decimal sharePrice = Convert.ToDecimal(Console.ReadLine());
+
+                Stock stock = new Stock();
+                stock.Name = name;
+                stock.NumShares = numShares;
+                stock.SharePrice = sharePrice;
+
+                portfolio.AddStock(stock);
+            }
+
+            portfolio.PrintReport();
+
+        }
+    }
+}
---
+++ StockAccountManagement/Stock.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccountManagement
+{
+    public class Stock
+    {
+        public string Name { get; set; }
+        public int NumShares { get; set; }
+        public decimal SharePrice { get; set; }
+
+        public decimal GetStockValue()
+        {
+            return NumShares * SharePrice;
+        }
+    }
+
+    public class StockPortfolio
+    {
+        List<Stock> stocks = new List<Stock>();
+
+        public void AddStock(Stock stock)
+        {
+            stocks.Add(stock);
+        }
+
+        public decimal GetTotalStockValue()
+        {
+            decimal totalStockValue = 0;
+
+            foreach (Stock stock in stocks)
+            {
+                totalStockValue += stock.GetStockValue();
+            }
+
+            return totalStockValue;
+        }
+
+        public void PrintReport()
+        {
+            Console.WriteLine("Stock Report:");
+            Console.WriteLine("\n**********\n");
+
+            foreach (Stock stock in stocks)
+            {
+                decimal value = stock.GetStockValue();
+                Console.WriteLine(stock.Name+" "+stock.NumShares+" shares at price "+stock.SharePrice+" : "+value);
+            }
+
+            decimal totalValue = GetTotalStockValue();
+            Console.WriteLine("\n*************\n");
+            Console.WriteLine("Total value of all stocks: "+totalValue);
+        }
+    }
+}
